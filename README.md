9e6bb6970233640bb263a28f5bcfc4a7e51a4b60
Lori Burleson <lori.burleson@gmail.com>
2016-09-24 12:31:01 -0500

    Removed unecessary files. Updated README for movie info file.

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 ## What To Watch
 ### A movie ratings library
 
-**To Run:**
-```rb
+**To Run Movie Info:**
+```bash
 $ ruby main.rb
 ```
+**To Run Movie Recommender:**
+```bash
+$ ruby movie_recommender.rb
+```
+
+**To Get Information About A Movie**
+In this mode, you will be able to search movies based on their ID number given in the Movie Lens database.
+
+You can also find the average ratings for a movie by giving the ID number and get all ratings a single user has left by supplying the user ID number. 
--- main.rb
+++ main.rb
@@ -1,12 +1,5 @@
-# This data set consists of:
-# 	* 100,000 ratings (1-5) from 943 users on 1682 movies.
-# 	* Each user has rated at least 20 movies.
-#         * Simple demographic info for the users (age, gender, occupation, zip)
 
 require 'csv'
-# require_relative 'user'
-# require_relative 'rating'
-# require_relative 'movie'
 require_relative 'file_reading'
 
 def find_movies_by_id(ratings_users_movie_ids, movie_id)
@@ -47,6 +40,16 @@ def avg_ratings_per_movie(ratings_users_movie_ids, movie_id)
   return all_ratings_average.round(2)
 end
 
+def find_ratings_per_user(ratings_users_movie_ids, user_id)
+  ratings_for_user = []
+  ratings_users_movie_ids.each do |movie|
+    if movie["user_id"] == user_id
+      ratings_for_user << movie["rating"].to_f
+    end
+  end
+  return ratings_for_user
+end
+
 def get_user_input
   print ' > '
   input = gets.chomp
@@ -72,22 +75,27 @@ def main
     puts "(5) Exit"
     user_mode_choice = get_user_input.to_i
 
-      if user_mode_choice == 4
+      if user_mode_choice == 1
         puts "Enter the Movie ID of your choice: "
         input = get_user_input
-        movie_title = find_movie_title(movie_ids_titles, input)
-        puts "That movie's title is: #{movie_title}"
+        averaged_ratings = avg_ratings_per_movie(ratings_users_movie_ids, input)
+        puts "Average of all ratings for the movie: #{averaged_ratings}"
+      elsif user_mode_choice == 2
+        puts "Enter the User ID of your choice: "
+        input = get_user_input
+        ratings_by_user_id = find_ratings_per_user(ratings_users_movie_ids, input)
+        puts "The user has left these ratings: #{ratings_by_user_id}"
       elsif user_mode_choice == 3
         puts "Enter the Movie ID of your choice: "
         input = get_user_input
         movie_title = find_movie_title(movie_ids_titles, input)
         ratings_per_movie = find_ratings_and_movies(ratings_users_movie_ids, input)
         puts "All ratings for your movie #{movie_title}: #{ratings_per_movie}"
-      elsif user_mode_choice == 1
-        puts "Enter the Movie ID of your choice: "
-        input = get_user_input
-        averaged_ratings = avg_ratings_per_movie(ratings_users_movie_ids, input)
-        puts "Average of all ratings for the movie: #{averaged_ratings}"
+      elsif user_mode_choice == 4
+      puts "Enter the Movie ID of your choice: "
+      input = get_user_input
+      movie_title = find_movie_title(movie_ids_titles, input)
+      puts "That movie's title is: #{movie_title}"
       elsif want_to_keep_going?(user_mode_choice)
         puts "See you next time!"
         exit
